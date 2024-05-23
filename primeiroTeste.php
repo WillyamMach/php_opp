f63e81530801ed5319ab9b7c78f49af2443ddb9a
Willyam Mach <willyam.mach@gmail.com>
2024-05-23 14:29:12 -0300

    Criando atributos

--- primeiroTeste.php
+++ primeiroTeste.php
@@ -3,3 +3,8 @@ require 'src/conta.php';
 
 $conta = new Conta();
 
+$conta->cpfTitular = '123.345.678.91';
+$conta->saldo = 399;
+$conta->nomeTitular = 'Willyam';
+
+var_dump($conta);
--- src/conta.php
+++ src/conta.php
@@ -1,4 +1,12 @@
 <?php
 class Conta {
+    public string $cpfTitular;
+    public float $saldo;
+    public string $nomeTitular;
 
+    // function __contruct($cpf, $saldo, $nomeTitular) {
+    //     this.$cpf = $cpf;
+    //     this.$saldo = $saldo;
+    //     this.$nomeTitular = $nomeTitular;
+    // }
 }
