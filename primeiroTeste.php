8de1bc8a46fa0658e6ecb1d93c1a4b42b6bbf8c8
Willyam Mach <willyam.mach@gmail.com>
2024-05-24 08:14:53 -0300

    Criando métodos

--- primeiroTeste.php
+++ primeiroTeste.php
@@ -7,4 +7,12 @@ $conta->cpfTitular = '123.345.678.91';
 $conta->saldo = 399;
 $conta->nomeTitular = 'Willyam';
 
+$conta_2 = new Conta();
+
+$conta_2->cpfTitular = '123.345.678.91';
+$conta_2->saldo = 100;
+$conta_2->nomeTitular = 'João';
+
+$conta->transferir(100, $conta_2);
 var_dump($conta);
+var_dump($conta_2);
--- src/conta.php
+++ src/conta.php
@@ -1,12 +1,33 @@
 <?php
 class Conta {
     public string $cpfTitular;
-    public float $saldo;
+    public float $saldo = 0;
     public string $nomeTitular;
 
-    // function __contruct($cpf, $saldo, $nomeTitular) {
-    //     this.$cpf = $cpf;
-    //     this.$saldo = $saldo;
-    //     this.$nomeTitular = $nomeTitular;
-    // }
+    public function sacar(float $valorSacar): void {
+        if ($valorSacar > $this->saldo) {
+            echo "Saldo Indisponível";
+            return;
+        } 
+        $this->saldo -= $valorSacar;
+
+    }
+    
+    public function depositar(float $saldoDepositar) {
+        if ($saldoDepositar > 0) {
+            $this->saldo += $saldoDepositar;
+            return;
+        } 
+        echo "Saldo Insuficiente!";
+    }
+
+    public function transferir(float $valorTransferir, $contaDestino): void {
+        if ($valorTransferir > $this->saldo) {
+            echo "Saldo Indisponível";
+            return;
+        } 
+
+        $this->sacar($valorTransferir);
+        $contaDestino->depositar($valorTransferir);     
+    }
 }
